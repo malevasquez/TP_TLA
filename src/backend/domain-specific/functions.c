fa3779eea5321b2095a8c1e650ca4edb89845d26
Azul <azkim@itba.edu.ar>
2022-06-11 18:54:23 -0300

    FEATURE: Added functions file
    Added getIntegerValue and getStringValue functions

---
+++ src/backend/domain-specific/functions.c
@@ -0,0 +1,9 @@
+#include "functions.h"
+
+void getIntegerValue(int value) {
+    dprintf(5, "%d", value);
+}
+
+void getStringValue(char* string) {
+    dprintf(5, "%s", string);
+}
---
+++ src/backend/domain-specific/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdio.h>
+
+void getIntegerValue(int value);
+void getStringValue(char* string);
+
+#endif
