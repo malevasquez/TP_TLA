565833c43a7f2ceb43c1a8ac5de731914f9907eb
Azul <azkim@itba.edu.ar>
2022-06-12 14:35:13 -0300

    PATCH: Fixed scopeTester

--- src/backend/data-structure/scopeTester.c
+++ src/backend/data-structure/scopeTester.c
@@ -11,37 +11,37 @@ int main(int argc, char const *argv[]) {
 
     scopeADT scope = newScope();
 
-    if(addDefinition(scope, INTEGER, "numero1") == -1)
+    if(addDefinition(scope, _INTEGER, "numero1") == -1)
         printf("ERROR: 1 \n");
 
-    if(addDefinition(scope, NOTE, "nota1") == -1)
+    if(addDefinition(scope, _NOTE, "nota1") == -1)
         printf("ERROR 2 \n");
 
-    if(addDefinition(scope, NOTE, "nota2") == -1)
+    if(addDefinition(scope, _NOTE, "nota2") == -1)
         printf("ERROR 3 \n");
 
-    if(addDefinition(scope, STRING, "nombre1") == -1)
+    if(addDefinition(scope, _STRING, "nombre1") == -1)
         printf("ERROR 4  ok \n");
 
 
     createSubScope(scope);
 
     
-    if(addDefinition(scope, INTEGER, "numero2") == -1)
+    if(addDefinition(scope, _INTEGER, "numero2") == -1)
         printf("ERROR 5 \n");
 
-    int numId = addDefinition(scope, INTEGER, "numero1");
+    int numId = addDefinition(scope, _INTEGER, "numero1");
     if( numId == -1)
         printf("ERROR 6 \n");
 
     int* num = (int*) malloc(sizeof(int));
     *num = 100; 
-    addAssignById(scope, numId, INTEGER, num);
+    addAssignById(scope, numId, _INTEGER, num);
 
     elem* aux = getElemByName(scope, "numero1");
     printf("%d %d %d %s  \n" ,aux->id, aux->type, *((int*)aux->value), aux->name);
 
-    if(addDefinition(scope, INTEGER, "numero2") == -1)
+    if(addDefinition(scope, _INTEGER, "numero2") == -1)
         printf("ERROR 7 > ok");
 
 
